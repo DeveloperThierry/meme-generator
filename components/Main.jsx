b1d8f20285899373b8477d4c5b41e257f5871fe6
developerthierry <developerthierry@gmail.com>
2025-11-22 05:55:30 +0000

    Controlled Components

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,30 +1,45 @@
-import {useState} from 'react'
+import { useState } from "react";
 export default function Main() {
-    const [meme, setMeme] = useState({
-        topText:"One does not simply",
-        bottomText:"Walk into Mordor",
-        imageUrl:"https://i.imgflip.com/1bij.jpg",
-    })
+  const [meme, setMeme] = useState({
+    topText: "One does not simply",
+    bottomText: "Walk into Mordor",
+    imageUrl: "https://i.imgflip.com/1bij.jpg",
+  });
+
+  function handleChange(e) {
+    const { value, name } = e.currentTarget;
+    setMeme((prevMeme) => ({ ...prevMeme, [name]: value }));
+  }
   return (
     <main>
       <div className="form">
         <label>
           Top Text
-          <input type="text" placeholder="One does not simply" name="topText" />
+          <input
+            type="text"
+            placeholder="One does not simply"
+            name="topText"
+            onChange={handleChange}
+            value={meme.topText}
+          />
         </label>
 
         <label>
           Bottom Text
-          <input type="text" placeholder="Walk into Mordor" name="bottomText" />
+          <input
+            type="text"
+            placeholder="Walk into Mordor"
+            name="bottomText"
+            onChange={handleChange}
+            value={meme.bottomText}
+          />
         </label>
         <button>Get a new meme image 🖼</button>
       </div>
       <div className="meme">
         <img src={meme.imageUrl} />
         <span className="top">{meme.topText}</span>
-        <span className="bottom">
-            {meme.bottomText}
-        </span>
+        <span className="bottom">{meme.bottomText}</span>
       </div>
     </main>
   );
